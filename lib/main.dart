b576dba6be853040fa0f97b30e84c42df98df613
iamirzashowvik <showvikmirza@gmail.com>
2021-05-20 11:19:54 +0100

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get.dart';
+
 import 'package:sms/sms.dart';
 
 void main() {
@@ -9,189 +12,205 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+    return GetMaterialApp(
       home: MyHomePage(),
+      builder: EasyLoading.init(),
     );
   }
-}class MyHomePage extends StatefulWidget {
+}
+
+class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- final _loginForm = GlobalKey<FormState>();
- SmsSender sender = new SmsSender();
+  final _loginForm = GlobalKey<FormState>();
+  SmsSender sender = new SmsSender();
   TextEditingController numbers = TextEditingController();
-  List<String> x=[];
+  List<String> x = [];
   TextEditingController code = TextEditingController();
+  int count = 0;
+  bool sendPermission=false;
   @override
   void initState() {
-    
     // TODO: implement initState
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-          body: SafeArea(
-                      child: Center(
-              child: Form(  key: _loginForm,
-                    child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                                      padding: EdgeInsets.symmetric(
-                                        horizontal: 30,
-                                      ),
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: TextFormField(
-                                          controller: numbers,
-                                          validator: (value) {
-                                            if (value.isEmpty) {
-                                              return 'numbers';
-                                            }
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Form(
+            key: _loginForm,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: numbers,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'numbers';
+                        }
 
-                                            return null;
-                                          },
-                                          decoration: new InputDecoration(
-                                            focusedBorder: OutlineInputBorder(
-                                                borderSide:
-                                                    BorderSide(color: Colors.white, width: 0.0),
-                                                borderRadius: BorderRadius.circular(10)),
-                                            enabledBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    color: Colors.grey[200], width: 0.0),
-                                                borderRadius: BorderRadius.circular(10)),
-                                            errorBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    color: Colors.red[300], width: 0.0),
-                                                borderRadius: BorderRadius.circular(10)),
-                                            focusedErrorBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    color: Colors.red[300], width: 0.0),
-                                                borderRadius: BorderRadius.circular(10)),
-                                            filled: true,
-                                            border: InputBorder.none,
-                                            hintText: 'numbers',
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                
-                                  Padding(
-                                      padding: EdgeInsets.symmetric(
-                                        horizontal: 30,
-                                      ),
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: TextFormField(
-                                          controller: code,
-                                          validator: (value) {
-                                            if (value.isEmpty) {
-                                              return 'code';
-                                            }
-
-                                            return null;
-                                          },
-                                          decoration: new InputDecoration(
-                                            focusedBorder: OutlineInputBorder(
-                                                borderSide:
-                                                    BorderSide(color: Colors.white, width: 0.0),
-                                                borderRadius: BorderRadius.circular(10)),
-                                            enabledBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    color: Colors.grey[200], width: 0.0),
-                                                borderRadius: BorderRadius.circular(10)),
-                                            errorBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    color: Colors.red[300], width: 0.0),
-                                                borderRadius: BorderRadius.circular(10)),
-                                            focusedErrorBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    color: Colors.red[300], width: 0.0),
-                                                borderRadius: BorderRadius.circular(10)),
-                                            filled: true,
-                                            border: InputBorder.none,
-                                            hintText:'code',
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                           Padding(
-                             padding: const EdgeInsets.all(30.0),
-                             child: GestureDetector(
-                                    onTap: () async {
-                                      if (_loginForm.currentState.validate()) {
-                                        
-print(numbers.text.split(' '));
-print(numbers.text.length);
-// int i=11;
-// print('object ${numbers.text.substring(i,i+11)}');
-// code.text.split(' ')
-print(code.text.split(' ').length);
-
-
-for(int i=0;i<numbers.text.length;i=i+11){
-setState(() {
-  x.add(numbers.text.substring(i,i+11));
-});
-
-}
- 
-
-
-                                      }}
-                                      ,child: Container(color: Colors.amber,child: Center(
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: Text('do it'),
-                                        ),
-                                      )),),
-                           ),
-                                      GestureDetector(
-                                  onTap: () async {
-                                    if (_loginForm.currentState.validate()) {
-                                      
-
-for(int i=0;i<340;i++){
-  
-// String ok=numbers.text.substring(i,i+11);
-  String address = x[i];
-  sender.sendSms(new SmsMessage(address, 'Dear MedXYour access code for General Anatomy is ${code.text.split(' ')[i]}'));
-}
- 
-
-
-                                    }}
-                                    ,child: Container(color: Colors.amber,child: Center(
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text('send'),
-                                      ),
-                                    )),),
-                 
-                  ],
+                        return null;
+                      },
+                      decoration: new InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.0),
+                            borderRadius: BorderRadius.circular(10)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey[200], width: 0.0),
+                            borderRadius: BorderRadius.circular(10)),
+                        errorBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.red[300], width: 0.0),
+                            borderRadius: BorderRadius.circular(10)),
+                        focusedErrorBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.red[300], width: 0.0),
+                            borderRadius: BorderRadius.circular(10)),
+                        filled: true,
+                        border: InputBorder.none,
+                        hintText: 'numbers',
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: code,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'message';
+                        }
+
+                        return null;
+                      },
+                      decoration: new InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.white, width: 0.0),
+                            borderRadius: BorderRadius.circular(10)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.grey[200], width: 0.0),
+                            borderRadius: BorderRadius.circular(10)),
+                        errorBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.red[300], width: 0.0),
+                            borderRadius: BorderRadius.circular(10)),
+                        focusedErrorBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.red[300], width: 0.0),
+                            borderRadius: BorderRadius.circular(10)),
+                        filled: true,
+                        border: InputBorder.none,
+                        hintText: 'Message',
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        numbers.clear();
+                        code.clear();
+                        count = 0;
+                        sendPermission=false;
+                      });
+                    },
+                    child: Text('Clear Text')),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: GestureDetector(
+                    onTap: () async {
+                      if (_loginForm.currentState.validate()) {
+                        print(numbers.text.split(' '));
+                        print(numbers.text.length);
+
+                        //print(code.text.split(' ').length);
+                        if (numbers.text.length % 11 != 0) {
+                          print('ki');
+                          Get.snackbar('Phone numbers are wrong',
+                              'ex: 017104270950164481915701710000000');
+                        } else {
+                          setState(() {
+                            sendPermission=true;
+                          });
+                          print('kok');
+                          for (int i = 0; i < numbers.text.length; i = i + 11) {
+                            setState(() {
+                              x.add(numbers.text.substring(i, i + 11));
+                              count++;
+                            });
+                          }
+                        }
+                      }
+                    },
+                    child: Container(
+                        color: Colors.amber,
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(sendPermission?'Accepted':'Analysis Numbers'),
+                          ),
+                        )),
+                  ),
+                ),
+               sendPermission? Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () async {
+                      int timer=code.text.split(' ').length;
+                      if (_loginForm.currentState.validate()) {EasyLoading.show(status: 'Please wait $timer Seconds');
+                        for (int i = 0; i < code.text.split(' ').length; i++) {
+                          String address = x[i];
+                          sender.sendSms(new SmsMessage(
+                              address,
+                              // 'Dear user,this is your code ${code.text.split(' ')[i]}')); //important for different text
+                              'Dear user,this is your code ${code.text}'));
+                          await Future.delayed(Duration(seconds: 1));
+
+                        }
+                      EasyLoading.dismiss();
+                        Get.snackbar("Congratulations", "You have sent all the texts.");
+                      }
+                    },
+                    child: Container(
+                        color: Colors.amber,
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Send Sms Now'),
+                          ),
+                        )),
+                  ),
+                ):Container(),
+              ],
             ),
           ),
-    )
-;
+        ),
+      ),
+    );
   }
 }
+
 extension on String {
   // ignore: unused_element
   List<String> splitByLength(int length) =>
